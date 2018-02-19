09e9a43243a62aca485af8e3b944b7667a3dc247
Filip Hedberg <filip.hedberg@trygve.se>
2018-02-19 08:38:01 -0800

    Setup mysql for project

--- suggestio/settings.py
+++ suggestio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'suggestio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'suggestio_db',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
