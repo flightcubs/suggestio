aeaf2874076276843564cff79af8a9d78b098ef4
Filip Hedberg <filip.hedberg@trygve.se>
2018-02-19 22:26:10 -0800

    Fix comment bug (comment object not passed on) after moving to class based view

--- suggestions/views.py
+++ suggestions/views.py
@@ -18,6 +18,13 @@ class DetailView(generic.DetailView):
     model = Suggestion
     template_name = 'suggestions/detail.html'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['comments'] = Comment.objects.filter(suggestion = self.kwargs['pk'])
+        return context
+
 def vote(request, suggestion_id):
     suggestion = get_object_or_404(Suggestion, pk=suggestion_id)
     suggestion.votes += 1
