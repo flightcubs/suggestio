3864ab9ff1f04e340658831aff5e70676581139f
Filip Hedberg <filip.hedberg@trygve.se>
2018-02-19 18:58:10 -0800

    Add CSS styling for detail page
    Add margin between footer and comment in detail view

--- suggestio/settings.py
+++ suggestio/settings.py
@@ -122,4 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+                os.path.join(BASE_DIR,'staticfiles'), # if your static files folder is named "staticfiles"
+)
+TEMPLATE_DIRS = (
+                os.path.join(BASE_DIR,'template'), # if your static files folder is named "template"
+)
+
+# For svg
+import mimetypes
+mimetypes.add_type("image/svg+xml", ".svg", True)
