2537e7c0f9f562342a7405a089b5190976282ff0
Filip Hedberg <filip.hedberg@trygve.se>
2018-02-20 10:23:39 -0800

    Add styling for list (index) page

--- suggestions/views.py
+++ suggestions/views.py
@@ -5,14 +5,22 @@ from django.views import generic
 
 from .models import Suggestion, Comment
 
+
+
 class IndexView(generic.ListView):
     template_name = 'suggestions/index.html'
     context_object_name = 'latest_suggestion_list'
+    model = Suggestion
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({
+            # Could pass more things here.
+        })
+        return context
 
     def get_queryset(self):
-        """Return the last published suggestions."""
-        return Suggestion.objects.order_by('-submit_date')[:10]
-
+        return Suggestion.objects.order_by('votes')
 
 class DetailView(generic.DetailView):
     model = Suggestion
@@ -21,7 +29,7 @@ class DetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
+        # Add in a QuerySet of all the comments for the current suggestion
         context['comments'] = Comment.objects.filter(suggestion = self.kwargs['pk'])
         return context
 
